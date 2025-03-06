02e47d99d5459637594c7d60da334ded8f4bbf6c
Phil Vukovic <pvukovic@device.local>
2025-03-06 16:16:12 -0500

    week 5

---
+++ week5/test.py
@@ -0,0 +1,13 @@
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .master("local[*]") \
+    .appName('test') \
+    .getOrCreate()
+
+df = spark.read \
+    .option("header", "true") \
+    .csv('taxi_zone_lookup.csv')
+
+df.show()
